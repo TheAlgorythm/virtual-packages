118836d14d02ddec2b9fa348ed3d9c37294b0c2c
ZSchoen <dev@zschoen.dev>
2021-06-09 11:32:05 +0200

    initial project begin

---
+++ test_plugins/sets.py
@@ -0,0 +1,21 @@
+def conjunct(first_list, second_list):
+    return any(map(lambda first: first in second_list, first_list))
+
+def disjunct(first_list, second_list):
+    return not any(map(lambda first: first in second_list, first_list))
+
+def not_in(value, block_list):
+    return value not in block_list
+
+def in_attr(value, in_list, attribute):
+    return any(map(lambda element: element[attribute] == value, in_list))
+
+def package_divide(package, manager, package_managers):
+    prioritized_managers = list(filter(lambda package_manager: package_manager in package, package_managers))
+    if not prioritized_managers:
+        return False
+    return prioritized_managers[0] == manager
+
+class TestModule(object):
+    def tests(self):
+        return {'conjunct': conjunct, 'disjunct': disjunct, 'not_in': not_in, 'in_attr': in_attr, 'package_divide': package_divide}
